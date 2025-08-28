c2da3f4f40cb278fbdd9bc4cd8fe637eef406499
Ace Rounds <roundsace@pal-nat186-74-118.itap.purdue.edu>
2025-08-28 12:46:44 -0400

    Added data cleaning section of Isomer project

---
+++ data_cleaning/ARL-Scripts/co60_Test/co60_first.ens_files/google-analytics.js
@@ -0,0 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+	dataLayer.push(arguments);
+}
+
+const ANALYTICS_ID = 'G-X8V4T08Q5G';
+
+gtag('js', new Date());
+gtag('config', ANALYTICS_ID);
